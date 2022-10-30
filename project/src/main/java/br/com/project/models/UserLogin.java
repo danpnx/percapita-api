ca5a7e72f9c625fe770dd03d7c362122fd171dfc
Anderson <anderson.c-morais@hotmail.com>
2022-10-30 17:27:49 -0300

    adicionado as configuracoes de seguranca para o usuario ter sua autenticacao ao site e app

---
+++ project/src/main/java/br/com/project/models/UserLogin.java
@@ -0,0 +1,35 @@
+package br.com.project.models;
+
+public class UserLogin {
+
+	private String username;
+
+	private String senha;
+
+	private String token;
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
