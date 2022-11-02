b8517452f5940709d24397b11d6052fbade07011
Daniel Augusto <danielpn23@outlook.com>
2022-11-02 00:28:21 -0300

    Changes in UserController class. Temporary url for endpoints

--- project/src/main/java/br/com/project/controller/UserController.java
+++ project/src/main/java/br/com/project/controller/UserController.java
@@ -2,44 +2,28 @@ package br.com.project.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.project.models.User;
 import br.com.project.service.UserService;
 
+import javax.validation.Valid;
+
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/register")
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	// somente para teste esse get irá sair
-	@GetMapping
+	// Endpoint para teste
+	@GetMapping("/all")
 	public ResponseEntity<List<User>> getAll() {
+		System.out.println("AT /USER/ALL ENDPOINT");
 		return ResponseEntity.ok(userService.findAll());
-	}
-
-	@PostMapping("/signup")
-	public ResponseEntity<Object> save(@RequestBody @Valid User userRegister) {
-		if (!userService.validatePassword(userRegister.getPassword())) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-					"Sua senha deve conter no minimo 8 caracteres a 20, com pelo menos 1 letra(s) maiúscula e ao menos 1 caractere(s) especial.");
-		}
-		if (userService.existsByUsername(userRegister.getUsername())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Email já está em uso.");
-		}
-		return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(userRegister));
 	}
 }
