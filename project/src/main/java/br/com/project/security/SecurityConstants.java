0fc01a3dfdb6a13e0ce19fa4d42de0a2df48ca60
Daniel Augusto <danielpn23@outlook.com>
2022-11-02 19:07:55 -0300

    changing the expiration time for the jwt token

--- project/src/main/java/br/com/project/security/SecurityConstants.java
+++ project/src/main/java/br/com/project/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package br.com.project.security;
 
 public class SecurityConstants {
 
-    public static final long EXPIRATION = 900_000;
+    public static final long EXPIRATION = 3_600_000;
     public static final String SECRET = "cHJvamV0byBpbnRlZ3JhZG9yIGRpZ2l0YWwgaG91c2UgZSBlbXBpcmljdXM=";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
