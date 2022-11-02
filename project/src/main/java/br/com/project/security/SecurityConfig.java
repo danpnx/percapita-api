4b868a597f72db5114310cbbde59974e8027112a
Daniel Augusto <danielpn23@outlook.com>
2022-11-02 00:31:59 -0300

    Changes in security configuration

--- project/src/main/java/br/com/project/security/SecurityConfig.java
+++ project/src/main/java/br/com/project/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,25 +21,27 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.sessionManagement().
-		sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().csrf().disable()
-		.cors();
-		http.authorizeHttpRequests((auth) -> auth
-				.antMatchers("/login/signin").permitAll()
-				.antMatchers("/register/signup").permitAll()
-				.antMatchers("/register/forgot-password").permitAll()
-				.antMatchers("/register/reset-password").permitAll()
+		// recupera o authenticationManager que está sendo compartilhado entre os objetos
+		AuthenticationManager authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+		http.cors().and()
+				.csrf().disable()
+				.authorizeRequests().antMatchers("/login").permitAll()
+				.antMatchers("/signup").permitAll()
+				.antMatchers("/password/*").permitAll()
 				.antMatchers(HttpMethod.OPTIONS).permitAll()
-				.anyRequest().authenticated()).httpBasic();
+				.anyRequest().authenticated()
+				.and()
+				.addFilter(new JwtAuthenticationFilter(authenticationManager))
+				.addFilterBefore(new JwtAuthorizationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		return http.build();
 	}
 }
