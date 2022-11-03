bef59febd54f8ee02824409a7c637b5c2b0ac2c8
Daniel Augusto <danielpn23@outlook.com>
2022-11-03 16:29:01 -0300

    Add relationship between financial transaction and user

--- project/src/main/java/br/com/project/service/FinancialTransactionService.java
+++ project/src/main/java/br/com/project/service/FinancialTransactionService.java
@@ -1,10 +1,17 @@
 package br.com.project.service;
 
+import br.com.project.enums.TransactionCategory;
 import br.com.project.models.FinancialTransaction;
+import br.com.project.models.User;
 import br.com.project.repositories.FinancialTransactionRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -12,17 +19,50 @@ import java.util.UUID;
 public class FinancialTransactionService {
 
     private final FinancialTransactionRepository transactionRepository;
+    private final UserService userService;
 
-    public FinancialTransactionService(FinancialTransactionRepository transactionRepository) {
+    public FinancialTransactionService(FinancialTransactionRepository transactionRepository, UserService userService) {
         this.transactionRepository = transactionRepository;
+        this.userService = userService;
     }
 
-    public void registerTransaction(FinancialTransaction transaction) {
+    public void registerTransaction(FinancialTransaction transaction, String username) {
+        User u = userService.findByUsername(username).get();
+        transaction.setUser(u);
         transactionRepository.save(transaction);
     }
 
     public void deleteTransaction(UUID transactionId) {
         // delete from the user's too
         transactionRepository.deleteById(transactionId);
+    }
+
+    public List<FinancialTransaction> getAllTransactions(String username) {
+        User u = userService.findByUsername(username).get();
+        return transactionRepository.findByUser(u);
+    }
+
+    public void editValue(BigDecimal value, UUID id) {
+        FinancialTransaction transaction = transactionRepository.findById(id).get();
+        transaction.setTransactionValue(value);
+        transactionRepository.save(transaction);
+    }
+
+    public void editCategory(TransactionCategory category, UUID id) {
+        FinancialTransaction transaction = transactionRepository.findById(id).get();
+        transaction.setTransactionCategory(category);
+        transactionRepository.save(transaction);
+    }
+
+    public void editDate(Date date, UUID id) {
+        FinancialTransaction transaction = transactionRepository.findById(id).get();
+        transaction.setTransactionDate(date);
+        transactionRepository.save(transaction);
+    }
+
+    public void editDescription(String description, UUID id) {
+        FinancialTransaction transaction = transactionRepository.findById(id).get();
+        transaction.setTransactionDescription(description);
+        transactionRepository.save(transaction);
     }
 }
