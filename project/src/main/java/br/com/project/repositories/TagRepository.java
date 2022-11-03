87fd063d3a5f7a9fca35210eedbd9a756466d42f
Daniel Augusto <danielpn23@outlook.com>
2022-11-02 21:33:36 -0300

    Revert "small change in tag repository"
    This reverts commit d4c22ab0dda8903d829fa27129507b5372ba94b6.

--- project/src/main/java/br/com/project/repositories/TagRepository.java
+++ project/src/main/java/br/com/project/repositories/TagRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
-    Optional<Tag> findByTagName(String name);
+    Optional<Tag> findTagByTagName(String name);
 }
