b03ffeaf28b82fc0cd5b62d5b35220af9d0d4181
Anderson <anderson.c-morais@hotmail.com>
2022-11-04 12:46:28 -0300

    adicionado o controller em relacao ao crud das tags

---
+++ project/src/main/java/br/com/project/controller/TagController.java
@@ -0,0 +1,47 @@
+package br.com.project.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.project.service.TagService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/tag")
+public class TagController {
+
+	@Autowired
+	private TagService tagService;
+
+	@PostMapping("/create")
+	public ResponseEntity<?> createTag(@RequestBody String tagName, Long userId) {
+		if (tagService.existsByTagNameAndUser(tagName, userId)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Já existe uma tag com este nome.");
+		}
+		tagService.registerTag(tagName, userId);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Tag " + tagName + " criada!");
+	}
+
+	@PutMapping("/edit")
+	public ResponseEntity<?> editTag(@RequestParam UUID tagId, String newName) {
+		tagService.editTag(tagId, newName);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Nome da tag atualizada.");
+	}
+
+	@DeleteMapping("/delete")
+	public ResponseEntity<?> deleteTag(@RequestParam String tagId) {
+		tagService.deleteTag(UUID.fromString(tagId));
+		return ResponseEntity.noContent().build();
+	}
+}
