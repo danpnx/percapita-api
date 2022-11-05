cd050261fa6a3b4e6d57a19f27c619940fb0ba7c
Daniel Augusto <danielpn23@outlook.com>
2022-11-05 11:50:46 -0300

    Creation of new method

--- project/src/main/java/br/com/project/repositories/FinancialTransactionRepository.java
+++ project/src/main/java/br/com/project/repositories/FinancialTransactionRepository.java
@@ -5,10 +5,13 @@ import br.com.project.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FinancialTransactionRepository extends JpaRepository<FinancialTransaction, UUID> {
     List<FinancialTransaction> findByUser(User user);
+
+    List<FinancialTransaction> findAllByTransactionDate(Date date);
 }
