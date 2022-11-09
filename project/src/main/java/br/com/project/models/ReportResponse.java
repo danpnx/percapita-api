331ae8672593ad58fafa381d3c7d968be38d33f6
Daniel Augusto <danielpn23@outlook.com>
2022-11-09 11:13:25 -0300

    Classe genérica para retornar dados de relatorio

---
+++ project/src/main/java/br/com/project/models/ReportResponse.java
@@ -0,0 +1,20 @@
+package br.com.project.models;
+
+import java.io.Serializable;
+
+public class ReportResponse<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private T responseValue;
+
+    public ReportResponse() {
+    }
+
+    public void setResponseValue(T value) {
+        this.responseValue = value;
+    }
+
+    public T getResponseValue() {
+        return responseValue;
+    }
+}
