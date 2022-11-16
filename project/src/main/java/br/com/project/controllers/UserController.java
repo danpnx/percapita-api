c403c945674f8d8ce6cfe6d794feed4b42832d7c
Daniel Augusto <danielpn23@outlook.com>
2022-11-16 00:14:56 -0300

    Adicionando swagger no controller de usuario

--- project/src/main/java/br/com/project/controllers/UserController.java
+++ project/src/main/java/br/com/project/controllers/UserController.java
@@ -1,11 +1,15 @@
 package br.com.project.controllers;
 
-import java.util.List;
-
+import br.com.project.models.ReportResponse;
 import br.com.project.utils.ContextUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.project.models.User;
@@ -14,6 +18,8 @@ import br.com.project.service.UserService;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/user")
+@Tag(name = "Informações do usuário", description = "Controller para retornar dados do usuário")
+@SecurityRequirement(name = "Bearer Authentication")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -24,6 +30,21 @@ public class UserController {
 //		return ResponseEntity.ok(userService.findAll());
 //	}
 
+	@Operation(
+			summary = "Perfil do usuário",
+			description = "Endpoint para retornar dados do usuário",
+			tags = {"Informações do usuário"},
+			responses = {
+					@ApiResponse(description = "OK", responseCode = "200", content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = User.class)
+					)),
+					@ApiResponse(description = "BAD REQUEST", responseCode = "400", content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = ReportResponse.class)
+					))
+			}
+	)
 	@GetMapping("/profile")
 	public ResponseEntity<User> getUser() {
 		String username = ContextUtils.getUsername();
