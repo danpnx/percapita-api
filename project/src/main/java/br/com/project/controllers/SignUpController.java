1e16901c6d1c7b0851bfd6d8b3b42b2d9b213d8d
Daniel Augusto <danielpn23@outlook.com>
2022-11-16 00:14:06 -0300

    Adicionando swagger no controller de criação de conta

--- project/src/main/java/br/com/project/controllers/SignUpController.java
+++ project/src/main/java/br/com/project/controllers/SignUpController.java
@@ -2,8 +2,15 @@ package br.com.project.controllers;
 
 import br.com.project.exceptions.DataNotAvailableException;
 import br.com.project.exceptions.InvalidInputException;
+import br.com.project.models.StandardMessage;
 import br.com.project.models.User;
 import br.com.project.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +23,30 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/signup")
+@Tag(name = "Sign up", description = "Controller para criação de conta no aplicativo")
 public class SignUpController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "Criar conta",
+            description = "Endpoint para criar a conta no aplicativo",
+            tags = {"Sign up"},
+            responses = {
+                    @ApiResponse(description = "BAD REQUEST", responseCode = "400", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardMessage.class)
+                    )),
+                    @ApiResponse(description = "CONFLICT", responseCode = "409", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardMessage.class)
+                    )),
+                    @ApiResponse(description = "CREATED", responseCode = "201", content = @Content(
+                            schema = @Schema(implementation = String.class)
+                    ))
+            }
+    )
     @PostMapping
     public ResponseEntity<?> signup(@RequestBody @Valid User user) {
         HttpStatus status =  userService.register(user);
