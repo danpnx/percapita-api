ed60e7484a2203a4fb1537eac704e5c5a8676f81
Daniel Augusto <danielpn23@outlook.com>
2022-11-16 00:11:22 -0300

    Adicionando anotação de segurança do swagger

--- project/src/main/java/br/com/project/config/OpenApiConfig.java
+++ project/src/main/java/br/com/project/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package br.com.project.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Utilize um token JWT para acessar a API. O token pode ser obtido ao realizar o login."
+)
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenApi() {
