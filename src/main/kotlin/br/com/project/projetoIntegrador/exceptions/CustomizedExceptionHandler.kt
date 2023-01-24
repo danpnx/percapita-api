cf6d59eec9bf363072ea43b12cadbae9349d9e5d
Daniel Augusto <danielpn23@outlook.com>
2023-01-24 09:32:37 -0300

    Anotacao em CustomizedExceptionHandler, alteracao em ReportService e classe UserSecurity

--- src/main/kotlin/br/com/project/projetoIntegrador/exceptions/CustomizedExceptionHandler.kt
+++ src/main/kotlin/br/com/project/projetoIntegrador/exceptions/CustomizedExceptionHandler.kt
@@ -5,9 +5,11 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import java.time.Instant
 
+@ControllerAdvice
 class CustomizedExceptionHandler {
 
     @ExceptionHandler(AuthorizationException::class)
