a6a09ce25940793a42d203f36b090260ba098a57
Anderson <anderson.c-morais@hotmail.com>
2023-01-26 19:49:12 -0300

    retirado imports descenessario da classe

--- src/main/kotlin/br/com/project/projetoIntegrador/ProjetoIntegradorApplication.kt
+++ src/main/kotlin/br/com/project/projetoIntegrador/ProjetoIntegradorApplication.kt
@@ -1,8 +1,6 @@
 package br.com.project.projetoIntegrador
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
