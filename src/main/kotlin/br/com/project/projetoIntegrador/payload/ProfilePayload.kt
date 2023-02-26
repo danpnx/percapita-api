c1b2f72b9d276fc3b7359ced8023b5ec04a3b72d
Anderson <anderson.c-morais@hotmail.com>
2023-02-26 15:52:16 -0300

    adicionado o atributo de password na classe

--- src/main/kotlin/br/com/project/projetoIntegrador/payload/ProfilePayload.kt
+++ src/main/kotlin/br/com/project/projetoIntegrador/payload/ProfilePayload.kt
@@ -4,4 +4,4 @@ package br.com.project.projetoIntegrador.payload
  * @project projetoIntegrador
  * @author Daniel Augusto on 10/02/2023
  **/
-data class ProfilePayload(val id: Long, val username: String, val name: String)
+data class ProfilePayload(val id: Long, val username: String, val name: String, val password: String) //adicionado o password
